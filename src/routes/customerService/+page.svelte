cc5546ee8094c41116f461b27caf180286ca6753
Rumyyyyy <dkfmadl1219@naver.com>
2024-11-12 18:22:22 +0900

    commit

--- src/routes/customerService/+page.svelte
+++ src/routes/customerService/+page.svelte
@@ -4,8 +4,8 @@
         <ul>
             <li>
                 <input type="radio" name="faq" id="q1">
-                <label class="question" for="q1">트립모먼트는 무엇인가요?</label>
-                <p class="answer">트립모먼트란 여행 중 찍은 사진만을 공유할 수 있는 공간입니다.</p>
+                <label class="question" for="q1">경로찾기와 경로추천의 차이는 무엇인가요?</label>
+                <p class="answer">경로찾기는 사용자가 경로를 짜기 쉽게 해주는 기능이고, <br>경로추천은 사용자의 조건에 따라 랜덤으로 여행 루트가 나오는 기능입니다.</p>
             </li>
             <li>
                 <input type="radio" name="faq" id="q2">
